8d7d4ca212b9b2d864125f69b648b3243e960523
C L <taxer.audits.0e@icloud.com>
2022-01-21 17:33:49 +0000

    Commiting public update.

--- public/js/secret.js
+++ public/js/secret.js
@@ -8,6 +8,10 @@
     elements.forEach((el) => {
       el.href = atob(el.dataset.secret);
       console.log("Secret Decoder", el.dataset.secret);
+      if (el.dataset.secrettext) {
+        el.innerHTML = atob(el.dataset.secrettext);
+        console.log("Secret Decoder Text", el.dataset.secrettext);
+      }
     });
   });
 })();
