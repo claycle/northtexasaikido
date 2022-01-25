e2530c5363a20672e2e89eed7a43e519ef24c9e9
C L <taxer.audits.0e@icloud.com>
2022-01-25 15:49:49 +0000

    Testing githook.

--- static/scripts/githook.php
+++ static/scripts/githook.php
@@ -1,12 +1,23 @@
 <?php
 //
 // A simple example of json in and out!!
+// SECRET: northtexasaikido.com => bm9ydGh0ZXhhc2Fpa2lkby5jb20=
 //
+$secret = "bm9ydGh0ZXhhc2Fpa2lkby5jb20=";
+
 $data = json_decode(file_get_contents('php://input'));
+
+$respond = "";
+if ($data['config']['secret'] == $secret) {
+    $respond = "Secret OK: " + $data['config']['secret'];
+}
+else {
+    $respond "Secret FAIL";
+}
 
 $a = [
     "hello" => "world",
-    "data" => $data
+    "response" => $respond
 ];
 
 echo json_encode($a);
