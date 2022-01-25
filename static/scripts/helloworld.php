7c18dba1f10014a5d58c1f660b28926bfe46fc28
C L <claycle@claycle.com>
2022-01-25 15:25:12 -0600

    Slightly more interesting helloworld.php

--- static/scripts/helloworld.php
+++ static/scripts/helloworld.php
@@ -2,12 +2,19 @@
 //
 // A simple example of json in and out!!
 //
-$data = json_decode(file_get_contents('php://input'));
+error_reporting(E_ERROR | E_PARSE);
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+// Converts it into a PHP object
+$data = json_decode($json, true);
+
+$response = "world";
+$rc = 200;
 
 $a = [
-    "hello" => "world",
+    "hello" => $response,
     "data" => $data
 ];
 
+http_response_code($rc);
 echo json_encode($a);
-http_response_code(200);
