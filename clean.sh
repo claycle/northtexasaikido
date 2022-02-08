619b0fa8873eb47911d4caa6f6a38778be30ccf4
C L <claycle@claycle.com>
2022-02-08 16:12:30 +0000

    Added clean.sh.

---
+++ clean.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# A simple script to either run the trash command line (macos, added via Macports or Brew)
+# or just rely on rm -rf
+
+
+DIR="./public"
+CMD=""
+if [ -d ${DIR} ]; then
+    eval "which -s xtrash"
+    if [ $? -eq 0 ]; then
+        CMD="trash ${DIR}/*"
+    else
+        CMD="rm -rf ${DIR}/*"
+    fi
+    eval ${CMD}
+else
+    echo "No $DIR"
+fi
+exit $?
