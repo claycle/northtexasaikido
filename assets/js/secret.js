492a2ea328a899c86177866dae487cb9e6b399e3
C L <claycle@claycle.com>
2022-02-21 15:18:05 +0000

    Changed console log levels.

--- assets/js/secret.js
+++ assets/js/secret.js
@@ -1,15 +1,15 @@
 window.addEventListener('load', (event) => {
-    console.log('secret.js Copyright (c) 2022 claycle.com');
+    console.info('secret.js Copyright (c) 2022 claycle.com');
 });
 
 window.addEventListener('load', (event) => {
     let elements = document.querySelectorAll("[data-secret]");
     elements.forEach(el => {
         el.href = atob(el.dataset.secret);
-        console.log("Secret Decoder", el.dataset.secret);
+        console.debug("Secret Decoder", el.dataset.secret);
         if (el.dataset.secrettext) {
             el.innerHTML = atob(el.dataset.secrettext);
-            console.log("Secret Decoder Text", el.dataset.secrettext);
+            console.debug("Secret Decoder Text", el.dataset.secrettext);
         }
     });
 });
--- assets/js/shuffle.js
+++ assets/js/shuffle.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', (event) => {
-    console.log('shuffle.js Copyright (c) 2022 claycle.com');
+    console.info('shuffle.js Copyright (c) 2022 claycle.com');
     var startProductBarPos = -1;
     //console.log("Today: {{ .Date }}, Title {{ site.Title }}");
 });
@@ -8,7 +8,7 @@ window.addEventListener('load', (event) => {
     // Locate all the data-shuffle images
     let elements = document.querySelectorAll("[data-shuffle]");
     // Fetch an image for this shuffle
-    // Images located with /images/shuffle 
+    // Images located with /images/shuffle
     // .Resources.ByType "image" //
     // resources.Match "images/shuffle/*"
     let deck = [];
@@ -16,11 +16,11 @@ window.addEventListener('load', (event) => {
     deck.push("{{ .RelPermalink }}");
     {{ end }}
     let shuffled = [];
-    
+
     // function shuffleArray(arr) {
     //   arr.sort(() => Math.random() - 0.5);
     // }
-    
+
     elements.forEach(el => {
         //console.log(el);
         if (shuffled.length === 0) {
